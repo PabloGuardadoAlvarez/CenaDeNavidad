d33403f518ef3ed9ad4d84e0e4bf7837aeedb693
PABLO GUARDADO ALVAREZ <pablo.guardado@utmac0018.local>
2019-01-09 08:33:08 -0800

    Initial Commit

---
+++ CenaNavidad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CenaNavidad
+//
+//  Created by PABLO GUARDADO ALVAREZ on 9/1/19.
+//  Copyright © 2019 PABLO GUARDADO ALVAREZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
